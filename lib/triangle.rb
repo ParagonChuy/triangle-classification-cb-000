65cbe8243b01963db706addd94ad6c39b14bfec4
Jesus Vega <chuyspc@gmail.com>
2017-01-11 09:01:06 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif (a == b) || (b == c) || (a == c)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    # No one side is greater than the sum of its two adjacent legs
+    triangle_cases = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each {|side| triangle_cases << false if side <= 0}
+    raise TriangleError if triangle_cases.include?(false)
+  end
+
+end
+
+class TriangleError < StandardError
 end
